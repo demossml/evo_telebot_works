5f06d217f4680579c7407e2c41f636eff2bbdd8a
Dima <demossml11@gmail.com>
2023-12-08 17:56:19 +0300

    "group_id"

--- reports/get_electro_sales.py
+++ reports/get_electro_sales.py
@@ -12,7 +12,7 @@ from collections import OrderedDict
 
 name = " 💨💨💨 Fyzzi/Электро ➡️".upper()
 desc = "Генерирует отчет по продажам в шт. по электронкам в шт"
-mime = 'text'
+mime = "text"
 
 
 def get_inputs(session: Session):
@@ -23,47 +23,58 @@ def generate(session: Session):
     shops = get_shops_user_id(session)
     shops_id = [v.uuid for v in shops]
     pprint(shops_id)
-    group_id = ["78ddfd78-dc52-11e8-b970-ccb0da458b5a", "bc9e7e4c-fdac-11ea-aaf2-2cf05d04be1d"]
+    group_id = [
+        "78ddfd78-dc52-11e8-b970-ccb0da458b5a",
+        "bc9e7e4c-fdac-11ea-aaf2-2cf05d04be1d",
+        "0627db0b-4e39-11ec-ab27-2cf05d04be1d",
+        "2b8eb6b4-92ea-11ee-ab93-2cf05d04be1d",
+        "8a8fcb5f-9582-11ee-ab93-2cf05d04be1d",
+        "97d6fa81-84b1-11ea-b9bb-70c94e4ebe6a",
+        "ad8afa41-737d-11ea-b9b9-70c94e4ebe6a",
+        "568905bd-9460-11ee-9ef4-be8fe126e7b9",
+        "568905be-9460-11ee-9ef4-be8fe126e7b9",
+    ]
 
     since = utcnow().replace(hour=3, minute=00).isoformat()
     until = utcnow().isoformat()
 
-    products = Products.objects(__raw__={
-        'parentUuid': {'$in': group_id},
-        'shop_id': {'$in': shops_id}
-    }).only('uuid')
+    products = Products.objects(
+        __raw__={"parentUuid": {"$in": group_id}, "shop_id": {"$in": shops_id}}
+    ).only("uuid")
 
     products_uuid = [element.uuid for element in products]
     result = []
     _dict = {}
     for shop in shops:
         result_shop = {}
-        shop_name = [i.name for i in Shop.objects(uuid=shop['uuid'])][0]
-        result_shop.update({'ТТ': shop_name})
-        documents = Documents.objects(__raw__={
-            'closeDate': {'$gte': since, '$lt': until},
-            'shop_id': shop['uuid'],
-            'x_type': 'SELL',
-            'transactions.commodityUuid': {'$in': products_uuid}
-        })
+        shop_name = [i.name for i in Shop.objects(uuid=shop["uuid"])][0]
+        result_shop.update({"ТТ": shop_name})
+        documents = Documents.objects(
+            __raw__={
+                "closeDate": {"$gte": since, "$lt": until},
+                "shop_id": shop["uuid"],
+                "x_type": "SELL",
+                "transactions.commodityUuid": {"$in": products_uuid},
+            }
+        )
 
         for doc in documents:
-            for trans in doc['transactions']:
+            for trans in doc["transactions"]:
                 # pprint(77)
-                if trans['x_type'] == 'REGISTER_POSITION':
+                if trans["x_type"] == "REGISTER_POSITION":
                     # pprint(88)
-                    if trans['commodityUuid'] in products_uuid:
+                    if trans["commodityUuid"] in products_uuid:
                         # pprint({trans['commodityName']: trans['quantity']})
-                        if trans['commodityName'] in _dict:
-                            _dict[trans['commodityName']] += trans['quantity']
-                            if trans['commodityName'] in result_shop:
-                                result_shop[trans['commodityName']] += trans['quantity']
+                        if trans["commodityName"] in _dict:
+                            _dict[trans["commodityName"]] += trans["quantity"]
+                            if trans["commodityName"] in result_shop:
+                                result_shop[trans["commodityName"]] += trans["quantity"]
                             else:
-                                result_shop[trans['commodityName']] = trans['quantity']
+                                result_shop[trans["commodityName"]] = trans["quantity"]
                         else:
                             # pprint({trans['commodityName']: trans['quantity']})
-                            _dict[trans['commodityName']] = trans['quantity']
-                            result_shop[trans['commodityName']] = trans['quantity']
+                            _dict[trans["commodityName"]] = trans["quantity"]
+                            result_shop[trans["commodityName"]] = trans["quantity"]
 
         # pprint(result_shop)
         if len(result_shop) > 1:
@@ -75,7 +86,7 @@ def generate(session: Session):
     total = 0
     for k, v in _dict.items():
         total += int(v)
-    _dict['Итого:'] = total
-    result.append({'⬇️Итого по всем тт'.upper(): '⬇️'.upper()})
+    _dict["Итого:"] = total
+    result.append({"⬇️Итого по всем тт".upper(): "⬇️".upper()})
     result.append(_dict)
     return result
